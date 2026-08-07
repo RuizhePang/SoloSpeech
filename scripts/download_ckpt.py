977b6af7cfdd52a7d897763c2ff4c79a23291ed5
RuizhePang <ruizhepang@gmail.com>
2026-08-07 14:20:47 +0800

    download ckpt

--- scripts/download_ckpt.py
+++ scripts/download_ckpt.py
@@ -2,5 +2,5 @@ from huggingface_hub import snapshot_download
 
 local_dir = snapshot_download(
     repo_id="OpenSound/SoloSpeech-models",
-    local_dir="./pretrained_models/original"
+    local_dir="../experiments/SoloSpeech/ckpts"
 )
