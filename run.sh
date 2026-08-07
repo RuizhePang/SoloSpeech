ef8ccdd67692a30cf7410190a3f7a5727f1a906f
RuizhePang <ruizhepang@gmail.com>
2026-08-07 21:23:37 +0800

    run.sh

--- run.sh
+++ run.sh
@@ -37,8 +37,8 @@ if [[ -f "$explog" ]]; then
     echo "Log file $explog already exists. Please remove it before running the script."
     exit 1
 fi
-echo "Running training with config: $config \
-    Logging to: $explog"
+echo "Running training with config: $config
+Logging to: $explog"
 "$cmd" "${scheduler_arguments[@]}" "$explog" \
     "$PYTHON_BIN" scripts/train/vae.py \
     --config-name="${config}" \
