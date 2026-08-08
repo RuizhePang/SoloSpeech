69f570f07b3627989eb8ce0420a2aa2ae6f30755
RuizhePang <ruizhepang@gmail.com>
2026-08-08 14:41:30 +0800

    ckpt

--- scripts/download_ckpt.py
+++ scripts/download_ckpt.py
@@ -3,21 +3,33 @@ from huggingface_hub import snapshot_download
 
 local_dir = snapshot_download(
     repo_id="OpenSound/SoloSpeech-models",
-    local_dir="../pretrained"
+    local_dir="../pretrained",
 )
 
-scr_dir = Path(local_dir).resolve()
-dst_dir = Path("../experiments/pretrained/ckpts").resolve()
+src_dir = Path(local_dir).resolve()
+dst_root = Path("../experiments/pretrained/ckpts").resolve()
 
-dst_dir.mkdir(parents=True, exist_ok=True)
+dst_root.mkdir(parents=True, exist_ok=True)
 
 for src in src_dir.glob("*"):
     if not src.is_file():
         continue
-    relative_path = src.relative_to(src_dir)
-    dst = dst_dir / relative_path
 
-    dst.parent.mkdir(parents=True, exist_ok=True)
+    name = src.name.lower()
+
+    if "compressor" in name:
+        dst_dir = dst_root / "compressor"
+    elif "extractor" in name:
+        dst_dir = dst_root / "extractor"
+    elif "corrector" in name:
+        dst_dir = dst_root / "corrector"
+    else:
+        # 不属于这三类的文件直接跳过
+        continue
+
+    dst_dir.mkdir(parents=True, exist_ok=True)
+
+    dst = dst_dir / src.name
 
     if dst.exists() or dst.is_symlink():
         dst.unlink()
