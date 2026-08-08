4f40b44ad95d4032ace732eeb4ba32ac4e918b93
RuizhePang <ruizhepang@gmail.com>
2026-08-08 13:25:34 +0800

    loguru

--- solospeech/stable_audio_vae/stable_audio_tools/training/stft_autoencoders.py
+++ solospeech/stable_audio_vae/stable_audio_tools/training/stft_autoencoders.py
@@ -1,5 +1,6 @@
 import torch
 import torchaudio
+from loguru import logger
 from einops import rearrange
 from safetensors.torch import save_file, save_model
 from ema_pytorch import EMA
@@ -349,6 +350,9 @@ class AutoencoderTrainingWrapper(pl.LightningModule):
             log_dict[f'train/{loss_name}'] = loss_value.detach()
 
         self.log_dict(log_dict, prog_bar=True, on_step=True)
+        logger_str = f"Step {self.global_step}: "
+        logger_str += ", ".join([f"{k}: {v.item():.6f}" for k, v in log_dict.items()])
+        logger.info(logger_str)
 
         return loss
     
